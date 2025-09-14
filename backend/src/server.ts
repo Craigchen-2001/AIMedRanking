80950e4a7b131cb9e898f69674852e77f4016a40
Craig Chen <chen.12915@osu.edu>
2025-09-14 13:36:40 -0400

    fix: listen on 0.0.0.0

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -50,6 +50,10 @@ app.use("/favorites", favoritesRouter);
 app.use("/api/favorites", favoritesRouter);
 
 const PORT = Number(process.env.PORT || 3001);
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
