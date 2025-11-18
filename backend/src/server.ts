b727f832376c886d1c0938b19bf06a7e30b7a5a4
Craig Chen <chen.12915@osu.edu>
2025-11-18 01:34:40 -0500

    fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,6 @@ app.use(
   cors({
     origin: [
       "https://aimedrank.aimedlab.net",
-      "https://aimedrank.aimedlab.net/api",
       "http://localhost:3000",
       "http://localhost:3001"
     ],
