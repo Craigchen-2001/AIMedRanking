0f1f2e5493c45ebadf2c7675ce8b8a6ae2b0cd55
Craig Chen <chen.12915@osu.edu>
2025-11-17 23:07:33 -0500

    Enable Next.js standalone mode

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const NEXT_PUBLIC_API_TARGET = process.env.API_NEXT_PUBLIC_API_TARGETTARGET || "http://localhost:3001";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   async rewrites() {
     return [
       {
